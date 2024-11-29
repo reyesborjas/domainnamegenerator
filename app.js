fcd70a57c53907ee56705c91689e33b14aaff843
reyesborjas86 <reyesborjas@gmail.com>
2024-11-29 14:01:30 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  
+  for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+          for (let k = 0; k < noun.length; k++) {
+              let domain = pronoun[i] + adj[j] + noun[k] +'.com';
+              console.log(domain);
+                      }
+          }
+  }
+  console.log("Hello Rigo from the console!");
+};
